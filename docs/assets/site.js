2e775f40da10ed6e2a7a04c66d72c6bcf4d2fab2
Garrett LeSage <garrett@lesage.us>
2016-09-29 16:50:37 +0200

    well, this method isn't as good... does it work enough yet?

--- docs/assets/site.js
+++ docs/assets/site.js
@@ -2,6 +2,3 @@
 # Be sure to not have a layout (no HTML wrapper for plain text)
 layout:
 ---
-{% include_relative assets/vendor/jquery/dist/jquery.js %}
-{% include_relative assets/vendor/jquery.easing/js/jquery.easing.js %}
-{% include_relative assets/vendor/bootstrap-sass/assets/javascripts/bootstrap.js %}
