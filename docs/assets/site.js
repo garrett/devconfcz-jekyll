b97e1b23492ca351012bf73939c12cc136f2b5ee
Garrett LeSage <garrett@lesage.us>
2016-09-29 16:26:27 +0200

    changed CSS; including some stuff via bower

---
+++ docs/assets/site.js
@@ -0,0 +1,7 @@
+---
+# Be sure to not have a layout (no HTML wrapper for plain text)
+layout:
+---
+{% include_relative vendor/jquery/dist/jquery.js %}
+{% include_relative vendor/jquery.easing/js/jquery.easing.js %}
+{% include_relative vendor/bootstrap-sass/assets/javascripts/bootstrap.js %}
---
+++ docs/js/jquery.easing.js
@@ -0,0 +1,1 @@
+../assets/vendor/jquery.easing/js/jquery.easing.js
