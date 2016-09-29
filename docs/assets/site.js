425b6faa50f4893c70f3d0d85f27e340005c0b7c
Garrett LeSage <garrett@lesage.us>
2016-09-29 16:43:10 +0200

    um, test?

--- docs/assets/site.js
+++ docs/assets/site.js
@@ -2,6 +2,6 @@
 # Be sure to not have a layout (no HTML wrapper for plain text)
 layout:
 ---
-{% include_relative vendor/jquery/dist/jquery.js %}
-{% include_relative vendor/jquery.easing/js/jquery.easing.js %}
-{% include_relative vendor/bootstrap-sass/assets/javascripts/bootstrap.js %}
+{% include_relative assets/vendor/jquery/dist/jquery.js %}
+{% include_relative assets/vendor/jquery.easing/js/jquery.easing.js %}
+{% include_relative assets/vendor/bootstrap-sass/assets/javascripts/bootstrap.js %}
